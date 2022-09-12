decfb2dbfbcba20d32a1ab83385547e55e78dcf0
Sean <41353165+seans-lab@users.noreply.github.com>
2022-09-12 17:40:09 +1000

    Update and rename 5-install-k6 to 5-install-k6.sh

--- scripts/5-install-k6.sh
+++ scripts/5-install-k6.sh
@@ -1,4 +1,7 @@
 # Install K6
+kubectl create namespace k6
+kubectl config set-context --current --namespace=k6
 git clone https://github.com/grafana/k6-operator && cd k6-operator
 make deploy
+kubectl config set-context --current --namespace=cafe
 kubectl create configmap crocodile-stress-test --from-file ../k6-test.js
