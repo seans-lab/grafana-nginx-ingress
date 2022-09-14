2096f340a039dd2ab35fc24fd1cf89d11f49e19d
Sean <41353165+seans-lab@users.noreply.github.com>
2022-09-15 08:21:59 +1000

    Update 1-install-nginx-ingress.sh

--- scripts/1-install-nginx-ingress.sh
+++ scripts/1-install-nginx-ingress.sh
@@ -1,6 +1,4 @@
 # Install NGINX Ingress
-kubectl create namespace nginx-ingress
-kubectl config set-context --current --namespace=nginx-ingress
 helm repo add nginx-stable https://helm.nginx.com/stable
 helm repo update
 helm install nginx-release nginx-stable/nginx-ingress --set prometheus.create=true --set prometheus.port=9113 --set prometheus.scheme=http
