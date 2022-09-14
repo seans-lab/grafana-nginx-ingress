9096aec726ae71c77a1fb9e066bd542c3232b245
Sean <41353165+seans-lab@users.noreply.github.com>
2022-09-15 08:22:55 +1000

    Update 3-install-grafana.sh

--- scripts/3-install-grafana.sh
+++ scripts/3-install-grafana.sh
@@ -1,6 +1,4 @@
 # INSTALL GRAFANA & PROMETHEUS
-kubectl create namespace grafana
-kubectl config set-context --current --namespace=grafana
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm install prometheus-kube prometheus-community/kube-prometheus-stack
 helm install prometheus prometheus-community/prometheus
